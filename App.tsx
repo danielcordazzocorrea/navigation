4e760b5f05e32c7a19ee332c33f950127fbfa045
Daniel Cordazzo Correa <danielcordazzocorrea@gmail.com>
2026-02-05 08:31:12 -0300

    app_started

--- App.tsx
+++ App.tsx
@@ -1,21 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Product } from "@/app/Product";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Teste!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Product />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
