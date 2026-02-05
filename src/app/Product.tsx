0bf5a6189fb0f5b0d5b2b5d7fb9756372ceb88df
Daniel Cordazzo Correa <danielcordazzocorrea@gmail.com>
2026-02-05 09:55:26 -0300

    making_routes

--- src/app/Product.tsx
+++ src/app/Product.tsx
@@ -3,11 +3,15 @@ import { Header } from '@/components/Header'
 import { Title } from '@/components/Title'
 import { View } from 'react-native'
 
+import { useNavigation } from '@react-navigation/native'
+
 export function Product() {
+  const navigation = useNavigation()
+
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
-        <ButtonIcon name="arrow-circle-left" />
+        <ButtonIcon name="arrow-circle-left" onPress={() => navigation.goBack()} />
         <Title>Product</Title>
       </Header>
     </View>
